5b2621d6d042d741472a90fe23a14a8e0cdac0c9
Kousha Amouzesh <ska387@sfu.ca>
2025-09-16 23:39:26 -0700

    lab2 implemenation

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,1 +1,50 @@
-int main(void) { return 1; }
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void) {
+  char *input = NULL;
+  size_t len = 0; // current allocated size for line
+  ssize_t nread;  // size for getline
+
+  while (1) {
+    printf("Enter programs to run.\n> ");
+
+    nread = getline(&input, &len, stdin);
+    
+    // input by default conatinas "\n..." we need to get rid of "\n" part
+    if (nread > 0 && input[nread - 1] == '\n') {
+      input[nread - 1] = '\0'; // remove new line
+    }
+
+    if (strlen(input) == 0) {
+      printf("Exiting program.\n");
+      break;
+    } else {
+      
+      // create a child process
+      pid_t pid = fork(); 
+      if (pid == -1) { // failed to fork? 
+        perror("forking failed"); 
+        exit(1); 
+      }
+        
+      // if child process successfully made
+      if (pid == 0) {
+        // executing the command 
+        execl(input, input, (char *)NULL); 
+        perror("Exec failure"); 
+      } else {
+        // waiting for the command 
+        int status; 
+        waitpid(pid, &status, 0); 
+      }
+    }
+  }
+
+  free(input);
+  return 1;
+}
