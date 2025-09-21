8f8ec76ae290b9bf5d4d3bbec525943fbc4aa151
Kousha Amouzesh <ska387@sfu.ca>
2025-09-21 10:23:30 -0700

    lab2 EXIT FALURE

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -14,7 +14,7 @@ int main(void) {
     printf("Enter programs to run.\n> ");
 
     nread = getline(&input, &len, stdin);
-    
+
     // input by default conatinas "\n..." we need to get rid of "\n" part
     if (nread > 0 && input[nread - 1] == '\n') {
       input[nread - 1] = '\0'; // remove new line
@@ -24,23 +24,23 @@ int main(void) {
       printf("Exiting program.\n");
       break;
     } else {
-      
+
       // create a child process
-      pid_t pid = fork(); 
-      if (pid == -1) { // failed to fork? 
-        perror("forking failed"); 
-        exit(1); 
+      pid_t pid = fork();
+      if (pid == -1) { // failed to fork?
+        perror("forking failed");
+        exit(EXIT_FAILURE);
       }
-        
+
       // if child process successfully made
       if (pid == 0) {
-        // executing the command 
-        execl(input, input, (char *)NULL); 
-        perror("Exec failure"); 
+        // executing the command
+        execl(input, input, (char *)NULL);
+        perror("Exec failure");
       } else {
-        // waiting for the command 
-        int status; 
-        waitpid(pid, &status, 0); 
+        // waiting for the command
+        int status;
+        waitpid(pid, &status, 0);
       }
     }
   }
